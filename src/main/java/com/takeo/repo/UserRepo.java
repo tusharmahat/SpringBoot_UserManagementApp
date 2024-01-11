d44dba4667254b8ba4f54b124a439ec255269775
tushar mahat <tusharmahat@gmail.com>
2024-01-10 18:15:50 -0700

    add create and read operations

--- src/main/java/com/takeo/repo/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.takeo.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.takeo.entity.User;
-
-public interface UserRepo extends JpaRepository<User, Integer> {
-
-}
